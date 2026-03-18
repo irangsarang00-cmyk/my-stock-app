9e4a49e9f36604330d0d012df8d1d45f111a0bdf
irangsarang00-cmyk <irangsarang00@gmail.com>
2026-03-18 21:10:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -491,11 +491,11 @@ elif st.session_state.current_page == "ecount":
         gb.configure_grid_options(suppressMovableColumns=True)
         
         # ✨ 2. 각 열의 크기를 지정! (이제 잘리지 않고 지정한 너비만큼 뻗어 나갑니다)
-        gb.configure_column('날짜', pinned='left', width=60) 
-        gb.configure_column('바코드', width=140)
+        gb.configure_column('날짜', pinned='left', width=100) 
+        gb.configure_column('바코드', width=130)
         gb.configure_column('제품명', width=500, wrapText=True, autoHeight=True) 
-        gb.configure_column('수량', width=70)
-        gb.configure_column('거래처', width=90)
+        gb.configure_column('수량', width=80)
+        gb.configure_column('거래처', width=110)
         
         gridOptions = gb.build()
         
