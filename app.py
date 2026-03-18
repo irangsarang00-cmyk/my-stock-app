9568f148f3615e6023208802648b70101c849a85
irangsarang00-cmyk <irangsarang00@gmail.com>
2026-03-18 23:57:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -645,21 +645,15 @@ elif st.session_state.current_page == "ecount":
     input_date = c1.date_input("일자", key="ecount_date").strftime("%Y%m%d")
     
     with c2:
-        c_lbl, c_btn = st.columns([7, 3])
-        with c_lbl:
-            st.markdown("<div style='font-size: 14px; margin-bottom: 5px; padding-top: 5px;'>거래처</div>", unsafe_allow_html=True)
-        with c_btn:
-            # 이제 위에서 덧씌운 디자인 규칙 덕분에 버튼이 쏙 작아집니다!
-            with st.popover("팁"):
-                st.markdown("✔️ <b>#만 있는 것</b> = 라온글로벌<br>✔️ <b>[YC]</b> = 우하모(야코브)", unsafe_allow_html=True)
-                
-        vendor_name = st.selectbox(
-            "거래처", 
-            list(vendor_list.keys()), 
-            key="ecount_vendor", 
-            label_visibility="collapsed"
-        )
-        vendor_code = vendor_list[vendor_name]
+    # 거래처 라벨 + 팁 버튼을 한 줄에 배치
+    label_col, tip_col = st.columns([8, 2])
+    label_col.markdown("<div style='font-size: 14px; padding-top: 5px;'>거래처</div>", unsafe_allow_html=True)
+    with tip_col:
+        with st.popover("팁"):
+            st.markdown("✔️ <b>#만 있는 것</b> = 라온글로벌<br>✔️ <b>[YC]</b> = 우하모(야코브)", unsafe_allow_html=True)
+    
+    vendor_name = st.selectbox("거래처", list(vendor_list.keys()), key="ecount_vendor", label_visibility="collapsed")
+    vendor_code = vendor_list[vendor_name]
     
     c3, c4 = st.columns(2)
     
